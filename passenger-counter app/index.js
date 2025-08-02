ceaaac2d77b23d6e73e46b5f76dd9807d6d8e36e
shadrackbond <stellamusembi10@gmail.com>
2025-08-03 00:32:17 +0300

    new assignments 1 and 2

---
+++ passenger-counter app/index.js
@@ -0,0 +1,8 @@
+//document.getElementById("count-el").innerText = 5
+//ctrl + k + c for commenting a group of code
+// let firstBatch = 5;
+// let secondBatch = 7;
+
+// let count = firstBatch + secondBatch;
+
+// console.log(count);
