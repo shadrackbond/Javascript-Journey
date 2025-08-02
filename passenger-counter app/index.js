0d304e34c9edef16ce09f1e6834bd6f3bd71ec60
shadrackbond <stellamusembi10@gmail.com>
2025-08-03 00:52:03 +0300

    event listener

--- passenger-counter app/index.js
+++ passenger-counter app/index.js
@@ -1,8 +1,11 @@
-//document.getElementById("count-el").innerText = 5
-//ctrl + k + c for commenting a group of code
-// let firstBatch = 5;
-// let secondBatch = 7;
+//document.getElementById("count-el").innerText = 5;
+//ctrl + k + c for commenting a group of code;
 
-// let count = firstBatch + secondBatch;
+//onclick event listener
+//initialize the count as 0
+//listen for clicks on the increment variable when the button is clicked
+//change the count-ek in the HTML to reflect the new count
 
-// console.log(count);
+function increment(){
+    console.log("the button was clicked");
+}
