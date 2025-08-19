95326989f1930dd3c691dd17a57d5a6651570c22
shadrackbond <stellamusembi10@gmail.com>
2025-08-19 21:16:59 +0300

    changes in file codes

--- passenger-counter app/index.js
+++ passenger-counter app/index.js
@@ -6,35 +6,23 @@
 //listen for clicks on the increment variable when the button is clicked
 //change the count-ek in the HTML to reflect the new count
 
-// let countEl = document.getElementById("count-el");//passing arguments to a function
+let countEl = document.getElementById("count-el");//passing arguments to a function
 
-// console.log(countEl);
+console.log(countEl);
 
-// let count = 0;
+let count = 0;
 
-// function increment(){
-//     count = count + 1;
-//     countEl.innerText = count;
-//     //console.log(count);
+function increment(){
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count);
 
-// }
-// //create a function, save(), which logs out the count when its called
-// function save(){
-//     console.log(count);
-// }
+}
+//create a function, save(), which logs out the count when its called
+ function save(){
+     console.log(count);
+}
 
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
-//Create tow variables(name and greeting) that contains your name
-// and the greeting we want to render on the page
-//Render the welcome page using welcomeEl.innerText
 
-let welcomeEl = document.getElementById("welcome-el");
-
-let name = "Shadrack Makau, ";
-let greeting = "Hello !! Welcome to the first of its kind coding journey";
-
-welcomeEl.innerText = name + greeting;
-
-welcomeEl.innerText += " emoji";
 
 
