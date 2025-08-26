73a398a0a11a67f4b24d313a77fb4970c922feeb
shadrackbond <stellamusembi10@gmail.com>
2025-08-26 16:25:27 +0300

    button and its functionality

--- Black-jack app/index.js
+++ Black-jack app/index.js
@@ -10,18 +10,18 @@ let sum = firstCard + secondCard;
 //if less than or equal to 20: do you want to draw a new card?
 //else if exactly 21: you have blackjack
 //else: you are out of the game
-
-if(sum <= 20){
-    message = "Do you want to draw a new card";
+function startGame(){
+    
+    if (sum <= 20) {
+        message = "Do you want to draw a new card";
+    }
+    else if (sum === 21) {
+        message = "Wow, you have a Blackjack";
+        hasBlackJACK = true;
+    }
+    else {
+        message = "you are out of the game";
+        isAlive = false;
+    }
+    console.log(message)
 }
-else if(sum === 21){
-    message = "Wow, you have a Blackjack";
-    hasBlackJACK = true;
-}
-else{
-    message = "you are out of the game";
-    isAlive = false;
-}
-console.log(hasBlackJACK)
-console.log(isAlive)
-console.log(message)
