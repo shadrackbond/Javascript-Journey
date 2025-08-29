4977e24ec6325d59595d3bc7247336e079b23556
shadrackbond <stellamusembi10@gmail.com>
2025-08-29 10:28:07 +0300

    starting on arrays

---
+++ Black-jack app/arrays.js
@@ -0,0 +1,15 @@
+//arrays - ordered lists of items 
+
+let featuredPosts = ["check out my netflix clone",
+                        "here is the code for my project",
+                        "I have just relaunched my portfolio"
+                    ]
+
+let experience = [
+    "CEO of SHAMAK industries",
+    "Backend developer",
+    "Robotics engineer at SHAMAK industries",
+    "Am a majorly self taught software engineer",
+    "I've worked on industry applications that have helped increase productivity",
+    "Most notable work is on the lost and found system, aimed at the task of retrieving and connecting"
+]
