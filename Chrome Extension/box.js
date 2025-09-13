c3a1fea0a444cc5331f4b2a362ea37c40a10f5f8
shadrackbond <stellamusembi10@gmail.com>
2025-09-13 11:01:37 +0300

    Started a new app, chrome extenstion. Initialised the required files, and have learnt to use addEventListeners instead of the onclick

---
+++ Chrome Extension/box.js
@@ -0,0 +1,5 @@
+let boxElement = document.getElementById("box");
+
+boxElement.addEventListener("click", function(){
+    console.log("I want to open the box!")
+})
---
+++ Chrome Extension/index.js
@@ -0,0 +1,10 @@
+// I use this when I have an onclick function in my html
+// function saveClicked(){
+//     console.log("Button has been clicked");
+// }
+
+let inputButton = document.getElementById("input-btn");
+
+inputButton.addEventListener("click", function(){
+    console.log("button clicked")
+})
