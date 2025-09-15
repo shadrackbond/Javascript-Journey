51c6b9e172fc5724261c392791ec624f17c9119c
shadrackbond <stellamusembi10@gmail.com>
2025-09-15 08:32:11 +0300

    using for loop to log out leads

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -2,13 +2,17 @@
 // function saveClicked(){
 //     console.log("Button has been clicked");
 // }
-let myLeads = [];
+let myLeads = ["www.getelement.com", "www.areReady.com", "www.musicSoul.com"];
 let inputEl = document.getElementById("input-el");
 
 let inputButton = document.getElementById("input-btn");
 
 inputButton.addEventListener("click", function(){
-    let x = document.getElementById("input-el").value;
-    myLeads.push(x)
+    myLeads.push(inputEl.value);
     console.log(myLeads);
 })
+
+for(let i =0; i< myLeads.length; i++){
+    console.log(myLeads[i]);
+}
+//using for loop to log out leads
