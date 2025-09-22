586bacc0b7cafd856817018a767677e6f1fcf735
shadrackbond <stellamusembi10@gmail.com>
2025-09-22 10:07:31 +0300

    Increasing performance by reducing the amount of DOM manipulation happening in a loop

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -2,19 +2,34 @@
 // function saveClicked(){
 //     console.log("Button has been clicked");
 // }
-let myLeads = ["www.getelement.com", "www.areReady.com", "www.musicSoul.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputButton = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputButton.addEventListener("click", function(){
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    renderLeads();
+    inputEl.value = "";
 })
 
-for(let i =0; i< myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> ";
+function renderLeads(){
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li> 
+
+                <a href = ${myLeads[i]} target = "_blank"> 
+                    ${myLeads[i]} 
+                </a> 
+
+            </li> `;
+
+        console.log(listItems)
+    }
+    ulEl.innerHTML = listItems;
 }
+
 //Use createElement() and append() instead of innerHTML
 //append means adding something to the end of a document
 // instead of innerHTML we could use createElement() this is how:
