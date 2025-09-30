f30e45d64f2a1af3edf396a5d7a5b791ee4ded3a
shadrackbond <stellamusembi10@gmail.com>
2025-09-30 09:25:28 +0300

    was having an issue with the tab function after debugging found out I had written tab instead of tabs in the query section

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -17,7 +17,7 @@ if(leadsFromLocalStorage){
 }
 
 tabBtn.addEventListener("click" ,function(){
-    chrome.tab.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url);
         localStorage.setItem("myLeads", JSON.stringify(myLeads));
         render(myLeads);
